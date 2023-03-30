6b2c00a7c985f646fcbc2526ac7867c55a69b8a4
mohanbendegumbali <115332018+mohanbendegumbali@users.noreply.github.com>
2023-03-30 20:12:35 +0530

    Update UserReg.java

--- src/main/java/UserReg.java
+++ src/main/java/UserReg.java
@@ -2,19 +2,17 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserReg {
-    static Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
-    public static void main(String[] args) {
-        System.out.println("Welcome to user registration regex problem!");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter first name starting with capital letter of at-least 3 character : \n");
-        String firstName = scanner.nextLine();
+public class UserRegUtility {
+    public boolean validateFirstName(String firstName) {
+        String validFirstName = "^[A-Z][a-zA-Z]{2,}";
+        Pattern pattern = Pattern.compile(validFirstName);
         Matcher matcher = pattern.matcher(firstName);
-        if ( matcher.matches()){
-            System.out.println("First name is valid!");
-        }else {
-            System.out.println("First is invalid!");
+        if (matcher.matches()) {
+            System.out.println("First Name is valid!!");
+            return true;
+        } else {
+            System.out.println("First Name is Invalid!!");
+            return false;
         }
-
-    }
+    
 }
