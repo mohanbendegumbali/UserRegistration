e6b14c76af3d11c53b190aeeb43652f4bc8d33bd
mohanbendegumbali <mohan0311@hotmail.com>
2023-03-31 20:34:19 +0530

    Parameterised Test

--- src/main/java/UserValidation.java
+++ src/main/java/UserValidation.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Uservalidation {
+public class UserValidation {
 
     public static boolean userFirstName(String firstName) {
 
