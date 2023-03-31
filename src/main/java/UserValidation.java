e4664f0d2e71639ec21fea1f7f68315a8a1e032e
mohanbendegumbali <mohan0311@hotmail.com>
2023-03-31 20:37:02 +0530

    Custom Exception

--- src/main/java/UserValidation.java
+++ src/main/java/UserValidation.java
@@ -1,115 +1,64 @@
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class UserValidation {
 
-    public static boolean userFirstName(String firstName) {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-        String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
+import java.util.Arrays;
+import java.util.Collection;
 
-        Pattern pattern = Pattern.compile(firstNamePattern);
-        Matcher matcher = pattern.matcher(firstName);
-        boolean result = matcher.matches();
-        return result;
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class EmailValidationTest {
+
+    private String emailTest;
+    private Boolean expectedResult;
+
+    private static UserValidation uservalidation;
+
+    public EmailValidationTest(String emailTest, Boolean expectedResult) {
+        this.emailTest = emailTest;
+        this.expectedResult = expectedResult;
     }
 
-    public static boolean userLastName(String lastName) {
-
-        String lastNamePattern = "[A-Z]{1}[a-z]{2,}";
-
-        Pattern pattern1 = Pattern.compile(lastNamePattern);
-        Matcher matcher1 = pattern1.matcher(lastName);
-        boolean result1 = matcher1.matches();
-        return result1;
+    @Before
+    public void initialize() {
+        uservalidation = new UserValidation();
     }
 
-    public static boolean userEmail(String email) {
-        String emailPattern = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*" + "@([0-9a-zA-Z][_]?)+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
-
-        Pattern pattern2 = Pattern.compile(emailPattern);
-        Matcher matcher2 = pattern2.matcher(email);
-        boolean result2 = matcher2.matches();
-        return result2;
-
+    @Parameterized.Parameters
+    public static Collection data() {
+        return Arrays.asList(new Object[][]{{"abc@yahoo.com", true},
+                {"abc-100@yahoo.com", true},
+                {"abc.100@yahoo.com", true},
+                {"abc111@abc.com", true},
+                {"abc-100@abc.net", true},
+                {"abc.100@abc.com.au", true},
+                {"abc@1.com", true},
+                {"abc@gmail.com.com", true},
+                {"abc+100@gmail.com", true},
+                {"ab@gmail.com", true},
+                {"abc", false},
+                {"abc@.com.my", false},
+                {"abc123@gmail.a", false},
+                {"abc123@.com", false},
+                {"abc123@.com.com", false},
+                {".abc@abc.com", false},
+                {"abc()*@gmail.com", false},
+                {"abc..2002@gmail.com", false},
+                {"abc@gmail.com.1a", false},
+                {"abc@gmail.com.aa.au", false}
+        });
     }
 
-    public static boolean mobileNumberValidation(String phoneNumber) {
-        Scanner scanner = new Scanner(System.in);
-        String MobileNumberPattern = "(91-)?[0-9]{10}";
 
-        Pattern pattern3 = Pattern.compile(MobileNumberPattern);
-        Matcher matcher3 = pattern3.matcher(phoneNumber);
-        Boolean result3 = matcher3.matches();
-        return result3;
-    }
-
-//    public static void passwordValidation() {
-//        Scanner scanner = new Scanner(System.in);
-//        String PasswordPattern = "[a-zA-Z0-9~!@#$]{8,}";
-//        System.out.println("Enter the Password");
-//        String passWord = scanner.next();
-//
-//        Pattern pattern4 = Pattern.compile(PasswordPattern);
-//        Matcher matcher4 = pattern4.matcher(passWord);
-//        Boolean result4 = matcher4.matches();
-//
-//        if (result4 == true) {
-//            passwordValidation2();
-//        } else {
-//            System.out.println("Password is invalid,please enter minimum 8 character");
-//        }
-//
-//    }
-//
-//    public static void passwordValidation2() {
-//        Scanner scanner = new Scanner(System.in);
-//        String PasswordPattern2 = "[A-Z]+[a-zA-Z0-9~!@#$]{7,}";
-//        System.out.println("Enter the Password to check Rule2");
-//        String passWord2 = scanner.next();
-//
-//        Pattern pattern5 = Pattern.compile(PasswordPattern2);
-//        Matcher matcher5 = pattern5.matcher(passWord2);
-//        Boolean result5 = matcher5.matches();
-//
-//        if (result5 == true) {
-//            passwordValidation3();
-//        } else {
-//            System.out.println("Password is invalid,please enter at least one upper case");
-//        }
-//
-//    }
-//
-//    public static void passwordValidation3() {
-//        Scanner scanner = new Scanner(System.in);
-//        String PasswordPattern3 = "[A-Z][0-9][a-zA-Z0-9~!@#$]{6,}";
-//        System.out.println("Enter the Password to check Rule3");
-//        String passWord3 = scanner.next();
-//
-//        Pattern pattern6 = Pattern.compile(PasswordPattern3);
-//        Matcher matcher6 = pattern6.matcher(passWord3);
-//        Boolean result6 = matcher6.matches();
-//
-//        if (result6 == true) {
-//            passwordValidation4();
-//        } else {
-//            System.out.println("Password is invalid,please enter at least one numeric");
-//        }
-//
-//    }
-
-    public static boolean passwordValidation4(String password4) {
-
-        String PasswordPattern4 = "([0-9a-zA-Z]*[~!@#$]){1}[0-9a-zA-Z~!@#$]*";
-
-        Pattern pattern7 = Pattern.compile(PasswordPattern4);
-        Matcher matcher7 = pattern7.matcher(password4);
-        boolean result7 = matcher7.matches();
-        return result7;
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to User Registration program");
+    @Test
+    public void givenUserEmail_WhenProper_ShouldReturnTrue() {
+        boolean result = uservalidation.userEmail(this.emailTest);
+        Assert.assertEquals("Email", this.expectedResult, result);
 
     }
 }
